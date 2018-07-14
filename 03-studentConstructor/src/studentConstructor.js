35e09265b5542fea762c854eb60b4f0545facee4
Indik47 <indik47@gmail.com>
2018-07-14 20:23:00 +0300

    refactored student constructor according to feedback

--- 03-studentConstructor/src/studentConstructor.js
+++ 03-studentConstructor/src/studentConstructor.js
@@ -1,58 +1,70 @@
 'use strict';
 
+var gradesSum = function (grades) {
+  return grades.reduce( (sum, grade) => sum += grade, 0);
+};
+
+var sortByAverage = function (a, b) {
+  return ( b.gradesAverage - a.gradesAverage );
+};
+
+var allStudentsSorted = function (allStudents) {
+  return allStudents.map(student => {
+    return {
+      name: student.name,
+      surname: student.surname,
+      grades: student.grades,
+      gradesAverage: student.gradesAverage()
+    }
+  }).sort(sortByAverage);
+};
+
+// constructor
+var Student = function(name, surname, grades) {
+  this.name = name;
+  this.surname = surname;
+  this.grades = grades;
+  this.fullName = function () {
+    return `${this.name} ${this.surname}`;
+  };
+
+  this.gradesAverage = function () {
+    return Number( (gradesSum( this.grades) / this.grades.length ).toFixed(2) );
+  };
+
+  if (!Student.group) { Student.group = []; }
+  Student.group.push(this);
+  Student.bestStudent = allStudentsSorted(Student.group)[0];
+};
+
+Student.showBestStudent = function () {
+  return `${Student.bestStudent.name} ${Student.bestStudent.surname} - кращий студент курсу. Середній бал = ${Student.bestStudent.gradesAverage}.`;
+};
+
+Student.showAllStudents = function () {
+  return allStudentsSorted(Student.group)
+    .map((student, key) =>
+      `${key}: ${student.name} ${student.surname}. Середній бал = ${student.gradesAverage}\n`)
+        .join("");
+ };
+
+//students creation
 var student1 = new Student('Ivan', 'Ivanov', [4,5,5]);
 var student2 = new Student('Semen', 'Semenov', [3,5,5]);
 var student3 = new Student('Petr', 'Petrov', [4,3,5]);
 var student4 = new Student('Irina', 'Morozova', [4,3,3]);
 var student5 = new Student('Katerina', 'Kolobkova', [5,5,5]);
 
-console.log(student1.fullName());
-console.log(student2.gradesAverage());
-console.log (Student.group());
+//function binding and logging
+var stud1Name = student1.fullName.bind(student1);
+var stud1Grades = student1.gradesAverage.bind(student1);
+console.log(stud1Name());
+console.log(stud1Grades());
+
+console.log (Student.group);
 console.log (Student.showAllStudents());
 console.log (Student.showBestStudent());
 console.log (Student.bestStudent);
 
 
-function Student(name, surname, grades) {
-  this.name = name;
-  this.surname = surname;
-  this.grades = grades;
-  this.fullName = function () {
-    return `${this.name} ${this.surname}`;
-  }
-  this.gradesAverage = function () {
-    const gradesSum = this.grades.reduce( (sum, grade) => sum += grade, 0);
-
-    return Number( (gradesSum/grades.length).toFixed(2) );
-  };
-
-  if (!Student.allStudents) {
-    Student.allStudents = [];
-  }
-
-  Student.allStudents.push(this);
-  // sort allStudents by grade
-  (function sortStudents(array){
-    array.sort(function(a,b) {return (a.gradesAverage() < b.gradesAverage()) ? 1 : ((b.gradesAverage() < a.gradesAverage()) ? -1 : 0);})
-  })(Student.allStudents);
-  // best student is 1st in the array
-  Student.bestStudent = Student.allStudents[0];
-
-  Student.group = function() {
-    return Student.allStudents;
-  };
-  Student.showAllStudents = function () {
-    let allStudents = '';
-
-    Student.allStudents.forEach(student => {
-      allStudents += `${student.fullName()}. Середній бал = ${student.gradesAverage()}.\n`;
-    });
-
-    return allStudents;
-  };
-  Student.showBestStudent = function () {
-    return `${Student.bestStudent.fullName()} - кращий студент курсу. Середній бал = ${Student.bestStudent.gradesAverage()}.`;
-  };
-}
 
