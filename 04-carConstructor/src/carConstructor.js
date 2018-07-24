ec10ad05a0fea924da5b18b6cf537d0ff2f5231c
Indik47 <indik47@gmail.com>
2018-07-24 13:47:16 +0300

    added styles to car Constructor

--- 04-carConstructor/src/carConstructor.js
+++ 04-carConstructor/src/carConstructor.js
@@ -1,135 +1,132 @@
 'use strict';
+var animTimer;
 
 var makeError = function (error) {
-  throw new Error(error);
+    price.value = error;
+    launchScaleAnimation(price, 'anim', 'error-anim');
+
+    throw new Error(error);
 };
 
 var Car = function (brand, model, color) {
-  this.brand = null;
-  this.model = null;
-  this.color = null;
-  this.options = [];
+    this.brand = null;
+    this.model = null;
+    this.color = null;
+    this.options = [];
 
-  if ( !(brand in Car.UNITS) ) {
-    makeError('Wrong brand')
-  };
-  this.brand = brand;
+    if (!(brand in Car.UNITS)) {
+        makeError('Brand not selected')
+    }
 
-  if ( !(model in Car.UNITS[brand]) ) {
-    makeError('Wrong model')
-  };
-  this.model = model;
+    this.brand = brand;
 
-  if ( !(color in Car.UNITS[brand][model]) ) {
-    makeError(`Color ${color} not available`);
-  };
-  this.color = color;
+    if (!(model in Car.UNITS[brand])) {
+        makeError('Model not selected')
+    }
+
+    this.model = model;
+
+    if (!(color in Car.UNITS[brand][model])) {
+        makeError(`Color not selected`);
+    }
+
+    this.color = color;
 };
 
 Car.UNITS = {
-  bmw: {
-    series3: {
-      black: {
-        quantity: 2,
-        price: 27880
-      },
-      silver: {
-        quantity: 3,
-        price: 29800
-      }
+    bmw: {
+        series3: {
+            black: {
+                quantity: 2,
+                price: 27880
+            },
+            silver: {
+                quantity: 3,
+                price: 29800
+            }
+        },
+        series5: {
+            black: {
+                quantity: 2,
+                price: 36900
+            },
+            royalBlue: {
+                quantity: 1,
+                price: 39800
+            }
+        },
     },
-    series5: {
-      black: {
-        quantity: 2,
-        price: 36900
-      },
-      royalBlue: {
-        quantity: 1,
-        price: 39800
-      }
+    audi: {
+        a3: {
+            black: {
+                quantity: 1,
+                price: 26340
+            },
+            white: {
+                quantity: 2,
+                price: 25400
+            }
+        },
+        a4: {
+            black: {
+                quantity: 2,
+                price: 38900
+            },
+            darkRed: {
+                quantity: 1,
+                price: 41200
+            }
+        }
     },
-  },
-  audi: {
-    a3: {
-      black: {
-        quantity: 1,
-        price: 26340
-      },
-      white: {
-        quantity: 2,
-        price: 25400
-      }
-    },
-    a4: {
-      black: {
-        quantity: 2,
-        price: 38900
-      },
-      darkRed: {
-        quantity: 1,
-        price: 41200
-      }
+    mercedes: {
+        classA: {
+            black: {
+                quantity: 1,
+                price: 28120
+            },
+            white: {
+                quantity: 2,
+                price: 29400
+            }
+        },
+        classC: {
+            black: {
+                quantity: 2,
+                price: 39500
+            },
+            darkRed: {
+                quantity: 1,
+                price: 45200
+            }
+        }
     }
-  },
-  mercedes: {
-    classA: {
-      black: {
-        quantity: 1,
-        price: 28120
-      },
-      white: {
-        quantity: 2,
-        price: 29400
-      }
-    },
-    classC: {
-      black: {
-        quantity: 2,
-        price: 39500
-      },
-      darkRed: {
-        quantity: 1,
-        price: 45200
-      }
-    }
-  }
 };
 Car.OPTIONS = {
-  panoRoof: {
-    price: 4650
-  },
-  leatherSeats: {
-    price: 3700
-  },
-  chromePaint: {
-    price: 1700
-  }
+    panoRoof: {
+        price: 4650
+    },
+    leatherSeats: {
+        price: 3700
+    },
+    chromePaint: {
+        price: 1700
+    }
 };
 
 Car.prototype.addOptions = function () {
-  document.querySelectorAll('.options__item-checkbox').forEach( item => {
-    if (item.checked) {
-      this.options.push(item.parentNode.textContent);
-    }
-  });
+    document.querySelectorAll('.options__item-checkbox').forEach(item => {
+        if (item.checked) {
+            this.options.push(item.parentNode.textContent);
+        }
+    });
 };
 
 Car.prototype.getPrice = function () {
-  var price = 0;
+    var price = 0;
 
-  price = Car.UNITS[this.brand][this.model][this.color].price;
-  price += this.options.reduce( (sum, option) => sum += Car.OPTIONS[option].price, 0);
+    price = Car.UNITS[this.brand][this.model][this.color].price;
+    price += this.options.reduce((sum, option) => sum += Car.OPTIONS[option].price, 0);
 
-  return price;
+    return price;
 };
-
-var car = {};
-
-document.getElementById('create-car').addEventListener('click', function () {
-  updateSelection();
-  car = new Car(selectedBrand, selectedModel, selectedColor);
-  car.addOptions();
-  updatePrice();
-});
-
 
--- 04-carConstructor/src/pageInit.js
+++ 04-carConstructor/src/pageInit.js
@@ -1,104 +1,125 @@
-var resetDropDown = function(dropDownID) {
-  var dropDownContainer = document.getElementById(dropDownID);
+var resetDropDown = function (dropDownID) {
+    var dropDownContainer = document.getElementById(dropDownID);
 
-  for (var i = dropDownContainer.options.length - 1; i >= 1; i--) {
-    dropDownContainer.remove(i);
-  }
-  dropDownContainer.options.selectedIndex = 0;
+    for (var i = dropDownContainer.options.length - 1; i >= 1; i--) {
+        dropDownContainer.remove(i);
+    }
+    dropDownContainer.options.selectedIndex = 0;
 };
 
 var fillDropDown = function (dropDownID, fillValues) {
-  var dropDownContainer = document.getElementById(dropDownID);
+    var dropDownContainer = document.getElementById(dropDownID);
 
-  for ( var key in Object.keys(fillValues) ) {
-    var dropDownItem = document.createElement("option");
-    dropDownItem.value = Object.keys(fillValues)[key];
-    dropDownItem.text = dropDownItem.value;
-    dropDownContainer.appendChild(dropDownItem);
-  }
+    for (var key in Object.keys(fillValues)) {
+        var dropDownItem = document.createElement("option");
+        dropDownItem.value = Object.keys(fillValues)[key];
+        dropDownItem.text = dropDownItem.value;
+        dropDownContainer.appendChild(dropDownItem);
+    }
 };
 
-var getMenuSelectedItem = function (dropDown) {
-  var dropDownMenu = document.getElementById(dropDown);
-  return dropDownMenu.options[dropDownMenu.selectedIndex].text;
+var getDropdownSelectedItem = function (dropDown) {
+    var dropDownMenu = document.getElementById(dropDown);
+    return dropDownMenu.options[dropDownMenu.selectedIndex].text;
 };
 
 var updateSelection = function () {
-  selectedBrand = getMenuSelectedItem('brands');
-  selectedModel = getMenuSelectedItem('models');
-  selectedColor = getMenuSelectedItem('colors');
+    selectedBrand = getDropdownSelectedItem('brands');
+    selectedModel = getDropdownSelectedItem('models');
+    selectedColor = getDropdownSelectedItem('colors');
+};
+
+var launchScaleAnimation = function (element, anim, animColor) {
+    element.classList.add(anim);
+    element.classList.add(animColor);
+    clearTimeout(animTimer);
+    animTimer = setTimeout(() => {
+        element.classList.remove(animColor);
+        animTimer = setTimeout(() => {
+            element.classList.remove(anim);
+        }, 1000);
+    }, 300);
 };
 
 var updatePrice = function () {
-  var price = document.getElementById('price');
-  price.value = car.getPrice();
+    var price = document.getElementById('price');
+    price.value = car.getPrice();
+
+    launchScaleAnimation(price, 'anim', 'sucess-anim');
 };
 
 var resetPrice = function () {
-  var price = document.getElementById('price');
-  price.value = '-' ;
-};
-
-var disableCreate = function () {
-  var button = document.getElementById('create-car');
-  button.disabled = true;
-};
-
-var enableCreate = function () {
-  var button = document.getElementById('create-car');
-  button.disabled = false;
+    var price = document.getElementById('price');
+    price.value = '';
 };
 
 var initOptions = function () {
-  for (var option in Car.OPTIONS) {
-    var label = document.createElement('label');
-    label.className = `options__item`;
-    label.textContent = `${option}`;
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = 'options__item-checkbox';
-    label.appendChild(checkbox);
+    for (var option in Car.OPTIONS) {
+        //parent item
+        var listItem = document.createElement('li');
+        listItem.textContent = `${option}`;
+        listItem.className = `check-container`;
 
-    var parentContainer = document.getElementById('options');
-    parentContainer.appendChild(label);
-  }
+        //span for "fake checkbox" overlay
+        var span = document.createElement('span');
+        span.className = `checkmark`;
+
+        //real checkbox
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.className = 'options__item-checkbox';
+
+        listItem.appendChild(checkbox);
+        listItem.appendChild(span);
+
+        var parentContainer = document.getElementById('options');
+        parentContainer.appendChild(listItem);
+    }
 };
 
+var car = {};
 var selectedBrand;
 var selectedModel;
 var selectedColor;
+
 fillDropDown('brands', Car.UNITS);
 initOptions(Car.OPTIONS);
-disableCreate();
-
 
 // event listeners
 //on brand change
-document.getElementById('brands').addEventListener( "change", function () {
-  updateSelection();
-  resetPrice();
-  disableCreate();
-  resetDropDown('colors');
-  resetDropDown('models');
-  fillDropDown( 'models', Car.UNITS[selectedBrand]);
-} );
-
-//on model change
-document.getElementById('models').addEventListener( "change", function () {
-  updateSelection();
-  resetPrice();
-  disableCreate();
-  resetDropDown('colors');
-  fillDropDown('colors', Car.UNITS[selectedBrand][selectedModel]);
-} );
-
-//on color change
-document.getElementById('colors').addEventListener( "change", function () {
-  updateSelection();
-  resetPrice();
-  enableCreate();
+document.getElementById('brands').addEventListener("change", function () {
+    updateSelection();
+    resetPrice();
+    resetDropDown('colors');
+    resetDropDown('models');
+    fillDropDown('models', Car.UNITS[selectedBrand]);
 });
 
+//on model change
+document.getElementById('models').addEventListener("change", function () {
+    updateSelection();
+    resetPrice();
+    resetDropDown('colors');
+    fillDropDown('colors', Car.UNITS[selectedBrand][selectedModel]);
+});
 
+//on color change
+document.getElementById('colors').addEventListener("change", function () {
+    updateSelection();
+    resetPrice();
+});
+
+//on create car click
+document.getElementById('create-car').addEventListener('click', function () {
+    try {
+        updateSelection();
+        car = new Car(selectedBrand, selectedModel, selectedColor);
+        car.addOptions();
+        console.log(car);
+        updatePrice();
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 
